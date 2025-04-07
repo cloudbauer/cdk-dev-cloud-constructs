6a58e6b09cab9cea3e2359dc48b551421837cc91
bitbauer <4582513+bitbauer@users.noreply.github.com>
2025-04-07 16:09:24 +0300

    ADD first construct and blueprint classes

---
+++ src/eks-cluster.ts
@@ -0,0 +1,95 @@
+import { Construct } from 'constructs';
+import { ArnPrincipal } from "aws-cdk-lib/aws-iam";
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as eks from 'aws-cdk-lib/aws-eks';
+
+// Blueprints Lib
+// import teams from '@aws-quickstart/eks-blueprints';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+
+// Team implementations
+class TeamPlatform extends blueprints.teams.PlatformTeam {
+    constructor(accountID: string) {
+        super({
+            name: "platform",
+            users: [new ArnPrincipal(`arn:aws:iam::${accountID}:user/superadmin`)],
+        });
+    }
+}
+
+/**
+ * Properties for the EksCluster construct
+ */
+export interface EksClusterConstructProps {
+    /**
+     * cluster base domain name
+     */
+    readonly domainName?: string;
+}
+  
+  
+/**
+ * Demonstrates how to leverage more than one node group along with Fargate profiles.
+ */
+
+export default class EksClusterConstructConstruct  {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+        const account = props?.env?.account!;
+        const region = props?.env?.region!;
+
+        // Setup platform team
+        const platformTeam = new TeamPlatform(account);
+ 
+        const stackID = `${id}-stack`;
+        
+        const clusterProvider = new blueprints.GenericClusterProvider({
+            version: eks.KubernetesVersion.V1_21,
+            managedNodeGroups: [
+                {
+                    id: "mng-ondemand",
+                    amiType: eks.NodegroupAmiType.AL2_X86_64,
+                    instanceTypes: [new ec2.InstanceType('m5.2xlarge')]
+                },
+                {
+                    id: "mng2-spot",
+                    instanceTypes: [ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MEDIUM)],
+                    nodeGroupCapacityType: eks.CapacityType.SPOT
+                }
+            ],
+            fargateProfiles: {
+                "fp1": {
+                    fargateProfileName: "fp1",
+                    selectors:  [{ namespace: "serverless1" }] 
+                },
+                "fp2": {
+                    fargateProfileName: "fp2",
+                    selectors:  [{ namespace: "serverless2" }] 
+                }
+            }
+        });
+
+        blueprints.EksBlueprint.builder()
+            .account(account)
+            .region(region)
+            .clusterProvider(clusterProvider)
+            .addOns(
+                new blueprints.AwsLoadBalancerControllerAddOn,
+                new blueprints.CertManagerAddOn,
+                new blueprints.AdotCollectorAddOn,
+                new blueprints.AppMeshAddOn,
+                new blueprints.NginxAddOn,
+                new blueprints.ArgoCDAddOn,
+                new blueprints.CalicoOperatorAddOn,
+                new blueprints.MetricsServerAddOn,
+                new blueprints.ClusterAutoScalerAddOn,
+                new blueprints.CloudWatchAdotAddOn,
+                new blueprints.XrayAdotAddOn,
+                new blueprints.SecretsStoreAddOn
+            )
+            .teams(platformTeam)
+            .version('auto')
+            .build(scope, stackID);
+    }
+}
---
+++ src/gitlab.ts
@@ -0,0 +1,41 @@
+import { Construct } from 'constructs';
+/*
+import {
+    Aspects,
+    CfnOutput,
+    Duration,
+    IAspect,
+    Stack,
+    Tags,
+} from 'aws-cdk-lib';
+import { Construct, IConstruct } from 'constructs';
+import { NagSuppressions } from 'cdk-nag';
+*/
+
+/**
+ * Properties for the Gitlab construct
+ */
+export interface GitlabProps {
+  /**
+   * Gitlab full qualified domain name
+   */
+  readonly domainName?: string;
+}
+  
+/**
+ * VSCodeServer - spin it up in under 10 minutes
+ */
+export class Gitlab extends Construct {
+  domainName: string;
+
+  constructor(scope: Construct, id: string, props?: GitlabProps) {
+    super(scope, id);
+
+    // defaults
+    this.domainName = props?.domainName ?? 'gitlab.example.com';
+  }
+
+  domain() {
+    return this.domainName;
+  }
+}
