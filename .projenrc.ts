91f445d236ac5c1426b1a623fb09673917daf4c0
bitbauer <4582513+bitbauer@users.noreply.github.com>
2025-04-07 19:49:46 +0300

    UPDATE dependecies versions to solve conflicts in Node class

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,21 +1,38 @@
 import { awscdk } from 'projen';
+
+// Constants
+const GITHUB_USER = 'cloudbauer';
+const PROJECT_NAME = 'cdk-dev-cloud-constructs';
+const CDK_VERSION: string = '2.173.4';
+
 const project = new awscdk.AwsCdkConstructLibrary({
-  name: 'cdk-dev-cloud-constructs',
+  name: PROJECT_NAME,
   description: 'CDK Construct Library to create an open source developer platform at AWS',
   author: 'bitbauer',
   authorAddress: '4582513+bitbauer@users.noreply.github.com',
   stability: 'experimental',
-  cdkVersion: '2.177.0',
+  cdkVersion: CDK_VERSION,
+  constructsVersion: '10.4.2',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.7.0',
   keywords: ['awscdk', 'gitlab', 'jenkins'],
+  deps: [
+    'cdk-nag',
+  ],
+  devDeps: [
+    '@aws-quickstart/eks-blueprints@1.16.3',
+  ],
+  peerDeps: [
+    'aws-cdk@^' + CDK_VERSION,
+    '@aws-quickstart/eks-blueprints@^1.16.3',
+  ],
 
   publishToPypi: {
     distName: 'cdk-dev-cloud-constructs',
     module: 'cdk_dev_cloud_constructs',
   },
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/cloudbauer/cdk-dev-cloud-constructs.git',
+  repositoryUrl: 'https://github.com/' + GITHUB_USER + '/' + PROJECT_NAME,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
