128f655b3e462c36413884cd0d03aecde5d4be55
bitbauer <4582513+bitbauer@users.noreply.github.com>
2025-04-14 20:21:15 +0200

    ADD projen subproject for builder lib

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,26 +1,22 @@
-import { awscdk } from 'projen';
+import { awscdk, typescript } from 'projen';
 
 // Constants
 const GITHUB_USER = 'cloudbauer';
 const PROJECT_NAME = 'cdk-dev-cloud-constructs';
 const CDK_VERSION: string = '2.173.4';
+const CDK_CONSTRUCTS_VERSION: string = '10.4.2';
 
-const project = new awscdk.AwsCdkConstructLibrary({
+const eksChartsConstructs = new awscdk.AwsCdkConstructLibrary({
   name: PROJECT_NAME,
   description: 'CDK Construct Library to create an open source developer platform at AWS',
-  author: 'bitbauer',
-  authorAddress: '4582513+bitbauer@users.noreply.github.com',
   stability: 'experimental',
   cdkVersion: CDK_VERSION,
-  constructsVersion: '10.4.2',
+  constructsVersion: CDK_CONSTRUCTS_VERSION,
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.7.0',
-  keywords: ['awscdk', 'gitlab', 'jenkins'],
+  keywords: ['awscdk', 'eks', 'kubernetes', 'gitlab', 'jenkins'],
   deps: [
     'cdk-nag',
-  ],
-  bundledDeps: [
-    '@aws-quickstart/eks-blueprints@1.16.3',
   ],
 
   publishToPypi: {
@@ -31,8 +27,37 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: 'https://github.com/' + GITHUB_USER + '/' + PROJECT_NAME,
 
   // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
+  author: 'bitbauer',
+  authorAddress: '4582513+bitbauer@users.noreply.github.com',
   license: 'MIT',
 });
-project.synth();
+
+const eksClusterBuilder = new typescript.TypeScriptProject({
+  parent: eksChartsConstructs,
+  name: 'cdk-dev-cloud-stack',
+  description: 'CDK Blueprint Builder for EKS Stack to create an open source developer platform at AWS',
+  defaultReleaseBranch: 'main',
+  stability: 'experimental',
+  keywords: ['awscdk', 'eks', 'kubernetes', 'gitlab', 'jenkins'],
+  projenrcTs: true,
+  deps: [
+    '@aws-quickstart/eks-blueprints@1.16.3',
+    'aws-cdk-lib@' + CDK_VERSION,
+    'constructs@' + CDK_CONSTRUCTS_VERSION,
+    'cdk-nag',
+  ],
+  peerDeps: [
+    'aws-cdk@' + CDK_VERSION,
+  ],
+
+  outdir: 'stack',
+  releaseToNpm: true,
+  release: true,
+
+  copyrightOwner: 'bitbauer',
+  license: 'MIT',
+});
+
+eksChartsConstructs.synth();
+eksClusterBuilder.synth();
