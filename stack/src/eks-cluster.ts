742b9acd6b088b377c1f0486ae25a86b9cdb102a
bitbauer <4582513+bitbauer@users.noreply.github.com>
2025-07-17 09:00:29 +0200

    feat: first implementation of testing for EKS stackbuilder (#15)
    ADD Stackbuilder and Props for EKS cluster
    ADD testing and more for Stackbuilder
    ADD jest configuration for ./stack subproject

--- stack/src/eks-cluster.ts
+++ stack/src/eks-cluster.ts
@@ -1,15 +1,13 @@
+import 'source-map-support/register';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
-import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as eks from 'aws-cdk-lib/aws-eks';
 import { ArnPrincipal } from 'aws-cdk-lib/aws-iam';
-import { Construct } from 'constructs';
+import { merge } from 'ts-deepmerge';
 
-// Blueprints Lib
-// import teams from '@aws-quickstart/eks-blueprints';
 
 // Team implementations
-class TeamPlatform extends blueprints.teams.PlatformTeam {
+export class TeamPlatform extends blueprints.teams.PlatformTeam {
   constructor(accountID: string) {
     super({
       name: 'platform',
@@ -19,46 +17,119 @@ class TeamPlatform extends blueprints.teams.PlatformTeam {
 }
 
 /**
- * Properties for the EksCluster construct
+ * Options for the EksClusterStackBuilder builder() constructor
  */
-export interface EksClusterStackProps extends cdk.StackProps {
+export interface EksClusterOptions {
   /**
-     * cluster base domain name
-     */
-  readonly domainName?: string;
+   * Cluster services base domain name
+   */
+  readonly domainName: string;
+
+  /**
+   * Required, Kubernetes version to use for the cluster.
+   */
+  kubernetesVersion?: eks.KubernetesVersion;
+  /**
+   * Required, Instance class to use for the cluster.
+   */
+  instanceClasses?: ec2.InstanceClass[];
+  /**
+   * Required, Instance size to use for the cluster.
+   */
+  instanceSizes?: ec2.InstanceSize[];
+  /**
+   * Optional, Desired number of nodes to use for the cluster.
+   */
+  desiredNodeSize?: number;
+  /**
+   * Optional, Minimum number of nodes to use for the cluster.
+   */
+  minNodeSize?: number;
+  /**
+   * Optional, Maximum number of nodes to use for the cluster.
+   */
+  maxNodeSize?: number;
+  /**
+   * Optional, Minimum number of spot nodes to use additionally for the cluster.
+   */
+  minSpotNodeSize?: number;
+  /**
+   * Optional, Block device size.
+   */
+
+  blockDeviceSize?: number;
+  /**
+   * Optional, Cluster Provider Tags.
+   */
+  clusterProviderTags?: {
+    [key: string]: string;
+  };
+
+  /**
+   * Optional, Node Group Tags for AL2 nodes
+   * which run standard cluster software.
+   */
+  nodeGroupTags?: {
+    [key: string]: string;
+  };
 }
 
+/**
+ * Default values to be used when creating the EKS cluster
+ */
+const defaultOptions: EksClusterOptions = {
+  domainName: 'undefined.com',
+  kubernetesVersion: eks.KubernetesVersion.of('1.32'),
+  instanceClasses: [ec2.InstanceClass.T3],
+  instanceSizes: [ec2.InstanceSize.MEDIUM, ec2.InstanceSize.LARGE],
+  desiredNodeSize: 2,
+  minNodeSize: 2,
+  maxNodeSize: 6,
+  minSpotNodeSize: 2,
+  blockDeviceSize: 50,
+  clusterProviderTags: {
+    Name: 'blueprints-bottlerock-eks-cluster',
+    Type: 'generic-bottlerock-cluster',
+  },
+  nodeGroupTags: {
+    Name: 'Mng-linux-bottlerock',
+    Type: 'Managed-linux-bottlerock-node-Group',
+    LaunchTemplate: 'Linux-Launch-Template',
+  },
+};
 
 /**
- * Demonstrates how to leverage more than one node group along with Fargate profiles.
+ * Leverage EKS cluster optimized for running all dev-cloud Constructs like Gitlab, Jenkins and Sonar
  */
 
-export class EksClusterStackBuilder {
-  constructor(scope: Construct, id: string, props?: EksClusterStackProps) {
+export class EksClusterStackBuilder extends blueprints.stacks.BlueprintBuilder {
 
-    const account = props?.env?.account!;
-    const region = props?.env?.region!;
-
-    // Setup platform team
-    const platformTeam = new TeamPlatform(account);
-
-    const stackID = `${id}-stack`;
+  /**
+   * This method helps you prepare a blueprint for setting up EKS cluster with
+   * Bottlerock image and addons for karpenter and monitoring
+   */
+  public static builder(options: EksClusterOptions): EksClusterStackBuilder {
+    const builder = new EksClusterStackBuilder();
+    const mergedOptions = merge(defaultOptions, options);
+    const nodeInstanceTypes = mergedOptions.instanceClasses?.map(category => mergedOptions.instanceSizes?.map(size => ec2.InstanceType.of(category, size)) ?? []).flat();
 
     const clusterProvider = new blueprints.GenericClusterProvider({
-      version: eks.KubernetesVersion.V1_31,
+      version: mergedOptions.kubernetesVersion,
+      tags: mergedOptions.clusterProviderTags,
       managedNodeGroups: [
         {
           id: 'mng-ondemand',
-          amiType: eks.NodegroupAmiType.AL2_X86_64,
-          instanceTypes: [new ec2.InstanceType('t3.medium')],
-          minSize: 3,
-          maxSize: 6,
+          amiType: eks.NodegroupAmiType.BOTTLEROCKET_X86_64,
+          instanceTypes: nodeInstanceTypes,
+          minSize: mergedOptions.minNodeSize,
+          desiredSize: mergedOptions.desiredNodeSize,
+          maxSize: mergedOptions.maxNodeSize,
         },
         {
           id: 'mng2-spot',
-          instanceTypes: [ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MEDIUM)],
+          instanceTypes: nodeInstanceTypes,
           nodeGroupCapacityType: eks.CapacityType.SPOT,
-          minSize: 3,
+          minSize: mergedOptions.minSpotNodeSize,
         },
       ],
       fargateProfiles: {
@@ -73,26 +144,26 @@ export class EksClusterStackBuilder {
       },
     });
 
-    blueprints.EksBlueprint.builder()
-      .account(account)
-      .region(region)
+    return builder
       .clusterProvider(clusterProvider)
       .addOns(
         new blueprints.AwsLoadBalancerControllerAddOn,
         new blueprints.CertManagerAddOn,
-        new blueprints.AdotCollectorAddOn,
-        new blueprints.AppMeshAddOn,
+        // new blueprints.AdotCollectorAddOn({
+        //   // namespace:'adot', //User supplied, non-default namespace
+        //   version: 'v0.80.0-eksbuild.2'
+        // }),
+        // new blueprints.AppMeshAddOn,
         new blueprints.NginxAddOn,
         // new blueprints.ArgoCDAddOn,
         // new blueprints.CalicoOperatorAddOn,
         new blueprints.MetricsServerAddOn,
-        new blueprints.ClusterAutoScalerAddOn,
-        new blueprints.CloudWatchAdotAddOn,
+        // new blueprints.ClusterAutoScalerAddOn({
+        //   version: '9.34.0'
+        // }),
+        //  new blueprints.CloudWatchAdotAddOn,
         // new blueprints.XrayAdotAddOn,
         new blueprints.SecretsStoreAddOn,
-      )
-      .teams(platformTeam)
-      .version('auto')
-      .build(scope, stackID);
+      );
   }
 }
